73fb09d5b9b9f4623db3fc700ca15743fd49ba92
TjD <konijn@gmail.com>
2012-05-29 20:59:18 -0400

    Lintified

--- src/js/js.js
+++ src/js/js.js
@@ -1,12 +1,21 @@
-
+/*
+ * js.js : all logic pertaining to extending standard js objects
+ *
+ * Copyright 2012 konijn@gmail.com aka Tom Demuyt
+ *
+ * Licensed under Apache v2.0 http://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ * jslint plusplus: true, sloppy: true, vars: true, white: true, maxerr: 10000 
+ */
 
 /* Whatever you do, do not extend Array ( boo ) */
 
+'use strict';
 
 String.prototype.condense = function()
 {
 	return this.split(" ").join("");
-}
+};
 
 String.prototype.capitalizeFirst = function()
 {
