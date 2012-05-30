fcda783d078067f2323d8c957540a883b8dfc01e
TjD <konijn@gmail.com>
2012-05-29 20:59:48 -0400

    lintified, no more getWidget

--- src/js/quintet.boxtext.js
+++ src/js/quintet.boxtext.js
@@ -6,62 +6,61 @@
  *
  * Licensed under Apache v2.0 http://www.apache.org/licenses/LICENSE-2.0.html
  *
+ * jslint plusplus: true, sloppy: true, vars: true, white: true, maxerr: 10000 
  */
 
-quintet.registerWidget
-(
-	widget = {
+'use strict';
 
-		id : "box",
+quintet.widgets.box =
+{
+  id : "box",
 
-		/* Mandatory : all widgets must have a createOptions */
-		/* These options will get serialized and stored in the backend for actual use */
-		createOptions : 	function()
-		{
-			var counter = $("."+this.id).length + 1;
-			if (counter==1) counter = "";
-			var o = quintet.getWidget("line").createOptions(); //<-- Lean on line
-			o.label = "Text " + counter;
-			o.hint = '';
-			o.required = false;
-			o.id = this.id;
-			o.ref = this.id + counter;
-			return o;
-		},
+  /* Mandatory : all widgets must have a createOptions */
+  /* These options will get serialized and stored in the backend for actual use */
+  createOptions :   function()
+  {
+    var counter = $("."+this.id).length + 1;
+    var o = quintet.widgets.line.createOptions(); //<-- Lean on line
+    o.label = "Text " + counter;
+    o.hint = '';
+    o.required = false;
+    o.id = this.id;
+    o.ref = this.id + counter;
+    return o;
+  },
 
-		//Really, I am expecting the id to be
-		//+1 for innerHTML
-		createOptionsUI : function ( id , element )
-		{
-			quintet.getWidget("line").createOptionsUI( id , element ); //<-- Lean on line
-		},
+  //Really, I am expecting the id to be
+  //+1 for innerHTML
+  createOptionsUI : function ( id , element )
+  {
+    quintet.widgets.line.createOptionsUI( id , element ); //<-- Lean on line
+  },
 
-		/* Mandatory : all widgets must have a create */
-		/* This is what the drag helper function calls, magic will place then the helper in the sortable form */
-		create : function( o /*options*/ )
-		{
-			//get options or create new options
-			//this gets messed up, hence the go-around for the original self
-			if( !o || (o instanceof jQuery.Event) )
-				o = quintet.getWidget("box").createOptions()
+  /* Mandatory : all widgets must have a create */
+  /* This is what the drag helper function calls, magic will place then the helper in the sortable form */
+  create : function( o /*options*/ )
+  {
+    //get options or create new options
+    //this gets messed up, hence the go-around for the original self
+    if( !o || (o instanceof jQuery.Event) )
+      o = quintet.widgets.box.createOptions();
 
-			//use the style options of line
-			quintet.getWidget("line").styleOptions( o );
+    //use the style options of line
+    quintet.widgets.line.styleOptions( o );
 
-			o.data = quintet.widget.encodeOptions( o );
+    o.data = quintet.widget.encodeOptions( o );
 
-			//Contrary to the original, I believe this to be
-			//more maintainable than coding all this with DOM manipulation
+    //Contrary to the original, I believe this to be
+    //more maintainable than coding all this with DOM manipulation
 
-			return $( sprintf('<div id="%(ref)s">%(_closeButton)s' +
-													'<input type="hidden" id="options" name="options" value=\'%(data)s\'>' +
-													'<div class="%(id)s widget">' +
-														'<label style="%(_style)s">%(_isRequired)s<span %(_labelColor)s >%(label)s</span></label>' +
-														'<textarea %(_valueColor)s rows="5" style="width:90%%">%(value)s</textarea>' +
-														'<span class="formHint" %(_hintColor)s>%(hint)s</span>' +
-													'</div>' +
-											'</div>' , o )
-				)
-		},
-	}
-);
+    return $( sprintf('<div id="%(ref)s">%(_closeButton)s' +
+                        '<input type="hidden" id="options" name="options" value=\'%(data)s\'>' +
+                        '<div class="%(id)s widget">' +
+                          '<label style="%(_style)s">%(_isRequired)s<span %(_labelColor)s >%(label)s</span></label>' +
+                          '<textarea %(_valueColor)s rows="5" style="width:90%%">%(value)s</textarea>' +
+                          '<span class="formHint" %(_hintColor)s>%(hint)s</span>' +
+                        '</div>' +
+                    '</div>' , o )
+      );
+  }
+};
