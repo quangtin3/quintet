8658f9e772b0212a19e2e9c0f36cfda522c46d5f
TjD <konijn@gmail.com>
2012-05-29 21:01:42 -0400

    lintified, no more getWidget

--- src/js/quintet.header.js
+++ src/js/quintet.header.js
@@ -8,68 +8,67 @@
  *
  */
 
-quintet.registerWidget
-(
-	widget = {
+'use strict';
 
-		id : "header",
+quintet.widgets.header =
+{
+  id : "header",
 
-		/* Mandatory : all widgets must have a createOptions */
-		/* These options will get serialized and stored in the backend for actual use */
-		createOptions : 	function()
-		{
-			var counter = $("."+this.id).length + 1;
-			var o = quintet.getWidget("line").createOptions(); //<-- Lean on line
-			o.label = "Header " + counter;
-			o.id = this.id;
-			o.ref = this.id + counter;
-			return o;
-		},
+  /* Mandatory : all widgets must have a createOptions */
+  /* These options will get serialized and stored in the backend for actual use */
+  createOptions :   function()
+  {
+    var counter = $("."+this.id).length + 1;
+    var o = quintet.widgets.line.createOptions(); //<-- Lean on line
+    o.label = "Header " + counter;
+    o.id = this.id;
+    o.ref = this.id + counter;
+    return o;
+  },
 
-		//Really, I am expecting the id to be
-		//+1 for innerHTML
-		createOptionsUI : function ( id , element )
-		{
+  //Really, I am expecting the id to be
+  //+1 for innerHTML
+  createOptionsUI : function ( id , element )
+  {
 
-			var o = quintet.widget.decodeOptions( element );
+    var o = quintet.widget.decodeOptions( element );
 
-			$("#"+id)[0].innerHTML =
-				quintet.htmlbuilder
-				.clear()
-				.h3("Basics")
-				.well()
-					.table()
-						.row()
-							.cell().label("label")
-							.cell("paddedStretch").textInput("label", o.label)
-				.html;
+    $("#"+id)[0].innerHTML =
+      quintet.htmlbuilder
+      .clear()
+      .h3("Basics")
+      .well()
+        .table()
+          .row()
+            .cell().label("label")
+            .cell("paddedStretch").textInput("label", o.label)
+      .html;
 
-				quintet.widget.current = o.ref;
+      quintet.widget.current = o.ref;
 
-		},
+  },
 
-		/* Mandatory : all widgets must have a create */
-		/* This is what the drag helper function calls, magic will place then the helper in the sortable form */
-		create : function( o /*options*/ )
-		{
-			//get options or create new options
-			//this gets messed up, hence the go-around for the original self
-			if( !o || (o instanceof jQuery.Event) )
-				o = quintet.getWidget("header").createOptions()
+  /* Mandatory : all widgets must have a create */
+  /* This is what the drag helper function calls, magic will place then the helper in the sortable form */
+  create : function( o /*options*/ )
+  {
+    //get options or create new options
+    //this gets messed up, hence the go-around for the original self
+    if( !o || (o instanceof jQuery.Event) )
+      o = quintet.widgets.header.createOptions();
 
-			//use the style options of line
-			quintet.getWidget("line").styleOptions( o );
+    //use the style options of line
+    quintet.widgets.line.styleOptions( o );
 
-			o.data = quintet.widget.encodeOptions( o );
+    o.data = quintet.widget.encodeOptions( o );
 
-			//Contrary to the original, I believe this to be
-			//more maintainable than coding all this with DOM manipulation
+    //Contrary to the original, I believe this to be
+    //more maintainable than coding all this with DOM manipulation
 
-			return $( sprintf('<div id="%(ref)s">%(_closeButton)s' +
-													'<input type="hidden" id="options" name="options" value=\'%(data)s\'>' +
-													'<h2 class="%(id)s widget">%(label)s</h2>' +
-											'</div>' , o )
-				)
-		},
-	}
-);
+    return $( sprintf('<div id="%(ref)s">%(_closeButton)s' +
+                        '<input type="hidden" id="options" name="options" value=\'%(data)s\'>' +
+                        '<h2 class="%(id)s widget">%(label)s</h2>' +
+                    '</div>' , o )
+      );
+  }
+};
