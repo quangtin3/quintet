b7485548aa878dcf6322d375672ad2bcc69abbc2
TjD <konijn@gmail.com>
2012-06-25 19:17:46 -0400

    Placeholder for now, will contain all the required backend APIs

---
+++ src/js/backend.js
@@ -0,0 +1,11 @@
+/*
+ * backend.js : collection of all backend calls
+ * leans on: quintet.singletextfield
+ *
+ * Copyright 2012 konijn@gmail.com aka Tom Demuyt
+ *
+ * Licensed under Apache v2.0 http://www.apache.org/licenses/LICENSE-2.0.html
+ *
+ */
+
+
